01a909003c0e0490fc52a733993bb3c6940bc7d5
isseium <issei@ikmz.net>
2016-06-20 23:56:30 +0900

    Add Infinity Scrollable View

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,3 +3,36 @@ function doClick(e) {
 }
 
 $.index.open();
+
+var views = $.sv.views;
+var viewCount = views.length;
+var viewCenterNumber = Math.ceil(views.length / 2) - 1;
+$.sv.currentPage = viewCenterNumber;
+
+var isScroll = false;
+$.sv.addEventListener('scroll', function(e){
+  Ti.API.debug(e.currentPage, e.currentPageAsFloat);
+
+  if(e.currentPage != e.currentPageAsFloat) return;
+  isScroll = true;
+
+  if(viewCenterNumber == $.sv.currentPage){
+    return;
+  }
+
+  if(e.currentPage > viewCenterNumber){
+    Ti.API.debug('next');
+    views.push(views.shift());
+  }else if(e.currentPage < viewCenterNumber){
+    Ti.API.debug('prev');
+    views.unshift(views.pop());
+  }
+
+  $.sv.currentPage = viewCenterNumber;
+});
+
+$.sv.addEventListener('scrollend', function(e){
+  Ti.API.debug('scrollend');
+  $.sv.setViews(views);
+  isScroll = false;
+});
